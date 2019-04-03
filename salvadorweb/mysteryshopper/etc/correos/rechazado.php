c70e87400056f2d5730b4294479b68796e37049d
AlexmgtGit <alejo.jesus.magne@gmail.com>
2019-04-03 09:04:29 -0400

    se agrego plantilla

---
+++ salvadorweb/mysteryshopper/etc/correos/rechazado.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns='http://www.w3.org/1999/xhtml'>
+<head>
+	<meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
+  <meta name='viewport' content='width=device-width, initial-scale=1' />
+  <title>Salvador Hairdressing - Mystery Shopper</title>
+	<style>
+		img { max-width: 600px; outline: none; text-decoration: none; -ms-interpolation-mode: bicubic;}
+    a img { border: none; }
+    table { border-collapse: collapse !important;}
+    #outlook a { padding:0; }
+    .ReadMsgBody { width: 100%; }
+    .ExternalClass { width: 100%; }
+    .backgroundTable { margin: 0 auto; padding: 0; width: 100% !important; }
+    table td { border-collapse: collapse; }
+    .ExternalClass * { line-height: 115%; }
+    .container-for-gmail-android { min-width: 600px; }
+    /* General styling */
+    * {
+      font-family: Helvetica, Arial, sans-serif;
+    }
+
+    body {
+      -webkit-font-smoothing: antialiased;
+      -webkit-text-size-adjust: none;
+      width: 100% !important;
+      margin: 0 !important;
+      height: 100%;
+      color: #676767;
+    }
+
+    td {
+      font-family: Helvetica, Arial, sans-serif;
+      font-size: 14px;
+      color: #777777;
+      text-align: center;
+      line-height: 21px;
+    }
+
+    a {
+      color: #676767;
+      text-decoration: none !important;
+    }
+
+    .pull-left {text-align: left;}
+
+    .pull-right {text-align: right;}
+
+    .header-lg{
+      font-size: 32px;
+      font-weight: 700;
+      line-height: normal;
+      padding: 35px 0 0;
+      color: #4d4d4d;
+    }
+    .content-padding {padding: 20px 0 10px;}
+
+    .mobile-header-padding-right {
+      width: 290px;
+      text-align: right;
+      padding-left: 10px;
+    }
+
+    .mobile-header-padding-left {
+      width: 290px;
+      text-align: left;
+      padding-left: 10px;
+    }
+
+    .free-text {
+      width: 100% !important;
+      padding: 10px 60px 0px;
+    }
+    .info-block {
+      padding: 0 20px;
+      width: 260px;
+    }
+    .force-width-gmail {
+      min-width:600px;
+      height: 0px !important;
+      line-height: 1px !important;
+      font-size: 1px !important;
+    }
+	</style>
+</head>
+
+<body bgcolor='#f7f7f7'>
+<table align='center' cellpadding='0' cellspacing='0' class='container-for-gmail-android' width='100%'>
+	<tr>
+		<td align='left' valign='top' width='100%' style='background:repeat-x url(http://s3.amazonaws.com/swu-filepicker/4E687TRe69Ld95IDWyEg_bg_top_02.jpg) #ffffff;'>
+		</td>
+		<center>
+		<img src='http://s3.amazonaws.com/swu-filepicker/SBb2fQPrQ5ezxmqUTgCr_transparent.png' class='force-width-gmail'>
+		<table cellspacing='0' cellpadding='0' width='100%' bgcolor='#ffffff' background='http://s3.amazonaws.com/swu-filepicker/4E687TRe69Ld95IDWyEg_bg_top_02.jpg' style='background-color:transparent'>
+			<tr>
+				<td width='100%' height='80' valign='top' style='text-align: center; vertical-align:middle;'>
+				<center>
+				<table cellpadding='0' cellspacing='0' width='600' class='w320'>
+					<tr>
+					<td class='pull-left mobile-header-padding-left' style='vertical-align: middle;'>
+					<a href='http://www.salvadorhairdressing.com/mysteryshopper'><img width='137' height='47' src='http://www.salvadorhairdressing.com/images/salv-hairdressing-logo.png'></a>
+					</td>
+					<td class='pull-right mobile-header-padding-right' style='color: #4d4d4d;'>
+					<a href='http://www.twitter.com/mundosalvador'><img width='33' height='32' src='http://www.salvadorhairdressing.com/images/rrss/twitter.jpg' /></a>
+					<a href='http://www.facebook.com/SalvadorWorld'><img width='33' height='32' src='http://www.salvadorhairdressing.com/images/rrss/facebook.jpg'/></a>
+					<a href='http://www.instagram.com/mundosalvador'><img width='33' height='32' src='http://www.salvadorhairdressing.com/images/rrss/instagram.jpg'  /></a>
+					<a href='https://www.youtube.com/user/salvadorpeluqueria'><img width='33' height='32' src='http://www.salvadorhairdressing.com/images/rrss/youtube.jpg' /></a>
+					</td>
+					</tr>
+				</table>
+				</center>
+				</td>
+			</tr>
+		</table>
+		</center>
+	</tr>
+</table>
+<tr>
+	<td align='center' valign='top' width='100%' style='background-color: #f7f7f7;' class='content-padding'>
+	<center>
+	<table cellspacing='0' cellpadding='0' width='600' class='w320'>
+		<tr>
+			<td class='header-lg'>
+			¡Solicitud Rechazada!
+			</td>
+		</tr>
+		<tr>
+			<td class='free-text'>
+			Tu solicitud de participación para ser un <b>Mystery Shopper</b> de <b><i>Salvador Hairdressing</i></b>, ha sido rechazada debido a que no cumples con los requisitos necesarios, por favor sigue intentando.
+			</td>
+		</tr>
+	</table>   
+	</center>  	
+</body>
+</html>
