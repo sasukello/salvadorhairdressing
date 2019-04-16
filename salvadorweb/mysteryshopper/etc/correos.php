5b91c58734f1637c4d108d1b3b4444e466e0208f
Alejandro Guerrero <alejo.jesus.magne@gmail.com>
2019-04-16 16:44:04 -0400

    cambios realizados

--- salvadorweb/mysteryshopper/etc/correos.php
+++ salvadorweb/mysteryshopper/etc/correos.php
@@ -73,7 +73,53 @@ function enviarMailBienvenida($emailDestino,$passDestino){
         <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
         $msg</div>";   
 }
+function enviarEmailFactura($idp){
 
+    $emailFac = getCorreo($idp);
+    require_once "../../mysteryshopper/etc/phpmailer/class.phpmailer.php";
+    require_once "../../mysteryshopper/etc/phpmailer/class.smtp.php";
+    
+    $mail = new PHPMailer(true);
+
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'mail.salvadorhairdressing.com';                  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'atc@salvadorhairdressing.com';    // SMTP username
+    $mail->Password = 'atencion.14';                         // SMTP password
+    $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 465;                                    // TCP port to connect to
+    $mail->CharSet = 'UTF-8';
+
+    $subject = "Salvador Hairdressing: Mystery Shopper - El participante $emailFac ha adjuntado su factura";
+    $email = 'noreply@salvadorhairdressing.com';
+    $name = 'Salvador Hairdressing';    
+
+    $mail->SetFrom( $email , $name );
+    $mail->AddAddress( 'prog.web@salvadorhairdressing.com' );
+    $archivo = $src;
+       
+    $mail->Subject = $subject;
+
+    $body = "Nueva factura de Mystery Shopper";
+
+    $mail->MsgHTML( $body );
+    $mail->AddAttachment($archivo);
+    $sendEmail = $mail->Send();
+
+    if( $sendEmail == true ):
+    $msg = "<b>¡Participante Aprobado Éxitosamente!</b> Este recibirá un correo siendo informado.<br><br><b>Aviso:</b> No se pueden programar visitas, hasta que el cliente no responda la encuesta inicial.";
+        $clase = "alert alert-success alert-dismissable fade in";
+        return "1";
+    else:
+            $msg = "<strong>Error</strong> al enviar Correo Informativo al Cliente.<br>";
+            $clase = "alert alert-danger alert-dismissable fade in";
+            return "0";
+    endif;
+    // echo "<div class='$clase'>
+    //     <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+    //     $msg</div>";   
+
+}
 function enviarEmailRechazado($emailRech){
 
     require_once "../../mysteryshopper/etc/phpmailer/class.phpmailer.php";
